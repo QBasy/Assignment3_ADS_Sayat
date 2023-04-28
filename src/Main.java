b5ba74f86cd38973d27277b24da3670b0650c703
QoidynBasy <c0mplexie1@gmail.com>
2023-04-28 19:18:01 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+
+    {
+        int a = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Stack (on LinkedList) - 1\nQueue (on ArrayList) - 2");
         int n = scanner.nextInt();
-        switch (n) {
+        switch (n)
+        {
             case 1:
-                System.out.println("Stack implementation\nPush - 1\nPop - 2\nPeek - 3\n");
-
+                while (a == 0)
+                {
+                    System.out.println("Stack implementation\nPush - 1\nPop - 2\nPeek - 3\nIs Empty - 4\nGet Size - 5");
+                    LinkedListStack LLStack = new LinkedListStack();
+                    switch (scanner.nextInt()) {
+                        case 1:
+                            System.out.println("how many pushes?");
+                            for (int i = 0; i < scanner.nextInt(); i++)
+                            {
+                                LLStack.pushMethod(scanner.next());
+                            }
+                            System.out.println("go back to the selection?\n 0 - yes\n 1 - no");
+                            a = scanner.nextInt();
+                            break;
+                        case 2:
+                            LLStack.popMethod();
+                            System.out.println("go back to the selection?\n 0 - yes\n 1 - no");
+                            a = scanner.nextInt();
+                            break;
+                        case 3:
+                            LLStack.peekMethod();
+                            System.out.println("go back to the selection?\n 0 - yes\n 1 - no");
+                            a = scanner.nextInt();
+                            break;
+                        case 4:
+                            LLStack.isEmpty();
+                            System.out.println("go back to the selection?\n 0 - yes\n 1 - no");
+                            a = scanner.nextInt();
+                            break;
+                        case 5:
+                            LLStack.getSize();
+                            System.out.println("go back to the selection?\n 0 - yes\n 1 - no");
+                            a = scanner.nextInt();
+                            break;
+                    }
+                }
+            case 2:
+                while (a == 0)
+                {
+                    System.out.println("Stack implementation\nPush - 1\nPop - 2\nPeek - 3\nIs Empty - 4\nGet Size - 5");
+                    switch (scanner.nextInt()) {
+                        case 1:
+                    }
+                }
         }
     }
 }
